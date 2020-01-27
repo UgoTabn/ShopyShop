7b82d046b67992e3b28f9313a8dd10fd79a548ff
DavidEtGeorges <59653406+DavidEtGeorges@users.noreply.github.com>
2020-01-27 11:38:41 +0100

    Create ShopyShop.c

---
+++ ShopyShop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Item {
+	
+	
+	
+	
+	
+}
+
+
+
+int main()
+{
+
+    return 0;
+}
+
+/* Ordre Alpha, Inventaire (quantitées),propose des articles, mecanique d'achat */
