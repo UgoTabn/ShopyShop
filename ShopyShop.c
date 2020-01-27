6a58f10e3c18c92e1c7e5cb7927d13c51d93ac40
DavidEtGeorges <59653406+DavidEtGeorges@users.noreply.github.com>
2020-01-27 12:48:31 +0100

    Update ShopyShop.c

--- ShopyShop.c
+++ ShopyShop.c
@@ -3,18 +3,52 @@
 
 struct Item {
 	int prix;
-	int premiereLettre;	
-}
+	int quat;	
+};
 typedef struct Item item;
 
 
 
-int main()
-{
-	item epee = {10, 5};
-	item bouclier = {10, 2};
-	item lance = {10, 12};
-	item baton = {10, 2};
+int main(){
+	int thunes = 100;
+	int choix = 0;
+	int achat = 0;
+	item vide = {"vide",0, 0}; 	// decla des items
+	item epee = {"epee",10, 5};
+	item bouclier = {"bouclier",10, 2};
+	item lance = {"lance",10, 12};
+	item baton = {"baton",10, 2};
+	item potion = {"potion",2, 22};
+	item ShopyShop[5];
+	ShopyShop[0] = vide;
+	ShopyShop[1] = epee;
+	ShopyShop[2] = bouclier;
+	ShopyShop[3] = lance;
+	ShopyShop[4] = baton;
+	ShopyShop[5] = potion;
+	
+	printf("Bienvenue dans le ShopyShop ! :^)\n");
+	printf("Vous desirez Acheter (1), ou vendre (2) ? \n");
+	printf("vous avez : %d\n",thunes);
+	scanf("%d", &choix);
+	epee.quat = 10;
+	if(choix == 1){
+		printf("____________________________________________________\n");
+		printf("%s,vaut %d, Quantite : %d\n",ShopyShop[1]);
+		printf("(1) pour acheter\n");
+		scanf("%d", &achat);
+		
+	}
+	if(achat == 1){
+		thunes = thunes - epee.prix;
+		epee.quat = epee.quat - 1;
+
+	 
+	}
+	                                                                   
+	printf("%s,Quantite : %d\n",epee.nom, epee.quat);
+	printf("vous avez : %d\n ",thunes);
+	
     return 0;
 }
 
